220844bebf5f965d6887b7c0ba9a94063338f664
mhdbaghrian <93444217+mhdbaghrian@users.noreply.github.com>
2022-06-01 09:54:24 +0430

    person with a lot of changes...

--- src/Model/Person.java
+++ src/Model/Person.java
@@ -10,34 +10,91 @@ import java.util.HashSet;
  */
 public class Person extends Requestable implements Serializable {
     private String passWord;
+    private String userName;
     private String email;
     private String phoneNumber;
     private Status status;
-    private HashSet<Person> friends;
-
+    private HashSet<String> friends;
+    private HashSet<String> blockList;
     private BufferedImage image;
 
-    public Person(String userName, String passWord, String email, String phoneNumber, Status status, HashSet<Person> friends) {
+    // Setters
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getters
+    public String getPassWord() {
+        return passWord;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public Status getStatus() {
+        return status;
+    }
+    public BufferedImage getImage() {
+        return image;
+    }
+    public HashSet<String> getFriends() {
+        return friends;
+    }
+    public HashSet<String> getBlockList() {
+        return blockList;
+    }
+
+    //constructors
+    public Person(String userName, String passWord, String email, String phoneNumber, Status status) {
         this.userName = userName;
         this.passWord = passWord;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.status = status;
-        this.friends = friends;
+        this.blockList = new HashSet<>();
+        this.friends = new HashSet<>();
     }
-
     public Person(String userName, String passWord, String email) {
         this.userName = userName;
         this.passWord = passWord;
         this.email = email;
+        this.blockList = new HashSet<>();
+        this.friends = new HashSet<>();
     }
 
-    public void addFriend(Person friend){
+    //add or remove to lists
+    public void addFriend(String friend){
         this.friends.add(friend);
     }
-
-    public void removeFriend(Person friend){
+    public void removeFriend(String friend){
         this.friends.remove(friend);
     }
+    public void block(String person){
+        this.blockList.add(person);
+    }
+    public void unblock(String person){
+        this.blockList.remove(person);
+    }
+
 }
 
