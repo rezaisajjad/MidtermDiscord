dc493cd325226030458f38e982fb8627b441aac2
Sajjad Rezai <rezai_sajjad@yahoo.com>
2022-07-11 10:44:57 +0430

    revise login and signup messages

--- Client/src/main/ClientController/Statics.java
+++ Client/src/main/ClientController/Statics.java
@@ -1,6 +1,6 @@
 package ClientController;
 
 public class Statics {
-    public static Integer MainPort = 11711;
-    public static Integer SecondPort = 11201;
+    public static Integer MainPort = 11223;
+    public static Integer SecondPort = 15154;
 }
